56c6ffcae825fcfddfe78f70e3eafd789b25baf9
unknown <sakshi.lonare@spit.ac.in>
2023-11-20 21:07:50 +0530

    configure github actions

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path=require('path');
 require("dotenv").config();
 
 const app = express();
@@ -14,7 +15,8 @@ app.use(cors({
   //origin: 'https://foodhubfront.netlify.app/',
 }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 async function start() {
   try {
