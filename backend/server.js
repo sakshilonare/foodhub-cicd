466dcd30ce967821cf8ee98f2e42495ec3501ced
Rucha-SK <124505906+Rucha-SK@users.noreply.github.com>
2023-11-21 09:38:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static("public"));
-//app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 
 async function start() {
   try {
